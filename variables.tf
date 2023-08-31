661b9fd5f12fb0eae91ddc7a212799fb91b89ea8
Sparkle <sparkleuyo@outlook.com>
2023-08-30 21:29:02 -0300

    terraform module

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-012fea7ccd0c03941"
+}
+variable "amiID" {
+	default="ami-026ebd4cfe2c043b2"
+}
+variable "keyPair" {
+	default="SpaceShuttle"
+}
+variable "keyPair1" {
+	default="SpaceShuttle.pem"
+}
+
+
+
+
